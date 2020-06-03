d876b4b60443d23d880b8d593cae01a4bbf1d31b
Daniel Niguse <Danielniguse8@gmail.com>
2020-06-03 23:47:09 +0100

    add social media icons

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./common.scss";
+import Github from "../../assets/images/github.svg";
+import Twitter from "../../assets/images/twitter.svg";
+import Facebook from "../../assets/images/facebook.svg";
 
 const Footer = () => {
   return (
     <div className="container-fluid footer-container">
       <div className="row">
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 footer-top">
-          footer
+          <div className="social-media-icon">
+            <Github className="github-logo" width={20} height={20} />
+            <Twitter className="twitter-logo" width={20} height={20} />
+            <Facebook className="twitter-logo" width={20} height={20} />
+          </div>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 footer-bottom">
-          footer
+          LOGO Copyright ©2020
         </div>
       </div>
     </div>
